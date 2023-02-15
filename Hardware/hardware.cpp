bc0eab476f4c82b1a42f0681b57edb38b0b8d740
Sergios Gavriilidis <sgavriilidis@hotmail.com>
2023-02-15 22:31:02 +0000

    Move Motion and Observation under Hardware

---
+++ Hardware/hardware.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+#include "ev3dev.h"
+#include "motion.h"
+#include "observation.h"
+
+// TODO: Can move motion and observation into 'ev3-hw' and add include scope
+//       Then we collect all fwd declares of ev3dev in a single header (minor improvement)
+
+using namespace motion;
+using namespace observation;
+
+int main() {
+  // Main control loop here
+  auto motors = MotorsWrapper{ev3dev::OUTPUT_D, ev3dev::OUTPUT_A};
+  auto sensor = USSensorWrapper{ev3dev::OUTPUT_C, ev3dev::INPUT_1};
+  unsigned mc = 0;  // Move command
+  while (true) {
+    std::cout << "Motion command: ";
+    std::cin >> mc;
+    std::cout << std::endl;
+    motors.drive(static_cast<Direction>(mc));
+  }
+  return 0;
+}
