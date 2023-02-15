db1f1011cb920b15d85e7041522ad94fdc755a0d
Sergios Gavriilidis <sgavriilidis@hotmail.com>
2023-02-15 22:17:04 +0000

    inline logging functions

--- Report/include/report.h
+++ Report/include/report.h
@@ -5,19 +5,17 @@
 
 namespace report {
 
-unsigned num_warns = 0;
-
 // TODO: Detect if logging is on, otherwise turn it off...
 #define Log(...) do { report::_log_message(std::cout, __VA_ARGS__); } while(0)
-// TODO: Add Error and Warn here...
 #define Assert(exp, ...) do { if (!(exp)) { report::_log_message(std::cerr, "Error: ", __VA_ARGS__, " in ", __FILE__, ":", __LINE__, '\n'); std::exit(1); } } while (0)
-#define Warn(exp, ...) do { if (!(exp)) { report::_log_message(std::cerr, "Warning:", __VA_ARGS__, " in ", __FILE__, ":", __LINE__, '\n'); report::num_warns++; } } while (0)
+#define Warn(exp, ...) do { if (!(exp)) { report::_log_message(std::cerr, "Warning:", __VA_ARGS__, " in ", __FILE__, ":", __LINE__, '\n'); } } while (0)
 
+// The below need to be inlined to adhere to ODR - this lib is used by many different translation units.
 // N.B. Implementation details - use the above macros, NOT the below functions!
-void _log_message(std::ostream& o){ (void)o; }
+inline void _log_message(std::ostream& o){ (void)o; }
 
 template<typename Head, typename ...Tail>
-void _log_message(std::ostream& o, Head&& h, Tail&& ...t) {
+inline void _log_message(std::ostream& o, Head&& h, Tail&& ...t) {
   o << std::forward<Head>(h);
   _log_message(o, std::forward<Tail>(t)...);
 }
