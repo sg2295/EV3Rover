165969854fd82c59be7d7ba6d8ba9951fa245a8d
Sergios Gavriilidis <sgavriilidis@hotmail.com>
2023-02-15 00:06:02 +0000

    Scan takes multiple readings from same pose in a 180 degree FOV

--- Observation/observation-test.cpp
+++ Observation/observation-test.cpp
@@ -1,18 +1,27 @@
 #include <iostream>
 
-#include "report.h"
-
 #include "observation.h"
 #include "ev3dev.h"
 
 using namespace observation;
 
 int main() {
-  Log("Starting observation test\n");
-  auto us_sensor = USSensorWrapper{ev3dev::OUTPUT_D, ev3dev::INPUT_1};
-  // TODO: Test multiscan later on...
-  while (true) {
-    Log("Reading: ", us_sensor.scan(), ".\n");
-  }
+  std::cout << "Starting observation test\n";
+  auto us_sensor = USSensorWrapper{ev3dev::OUTPUT_C, ev3dev::INPUT_1};
+  // while (true) {
+  //   std::cout << "pos=" << us_sensor.motor->position() << std::endl;
+  // }
+  // Test static sensing...
+  auto sensor_readings = us_sensor.scan();
+  for (auto i : sensor_readings)
+    std::cout << i << ", ";
+  std::cout << std::endl;
+  int x = 0;
+  std::cin >> x;
+  std::cout << std::endl;
+  sensor_readings = us_sensor.scan();
+  for (auto i : sensor_readings)
+    std::cout << i << ", ";
+  std::cout << std::endl;
   return 0;
 }
--- Observation/observation.cpp
+++ Observation/observation.cpp
@@ -1,4 +1,8 @@
 #include <memory>
+#include <chrono>
+#include <thread>
+
+#include "report.h"
 
 #include "observation.h"
 #include "ev3dev.h"
@@ -7,23 +11,43 @@ namespace observation {
 
 using MM = ev3dev::medium_motor;
 using USS = ev3dev::ultrasonic_sensor;
+using SensorReading = std::array<float, USSensorWrapper::num_measurements>;
 
 USSensorWrapper::USSensorWrapper(ev3dev::address_type us_motor_add, ev3dev::address_type us_sensor_add) :
-  motor{std::make_unique<MM>(us_motor_add)}, us_sensor{std::make_unique<USS>(us_sensor_add)} {}
+  motor{std::make_unique<MM>(us_motor_add)}, us_sensor{std::make_unique<USS>(us_sensor_add)} {
+    Assert(us_sensor->connected(), "Sensor not connected");
+    Assert(motor->connected(), "Motor not connected");
+    // Reset motor to starting position...
+    motor->set_position_sp(m_start_pos);
+    motor->set_speed_sp(m_speed).run_to_abs_pos();
+    while (motor->state().count("running"))
+      std::this_thread::sleep_for(std::chrono::milliseconds(10));
+    Assert(motor->position() == m_start_pos, "Motor position invalid in ctor.");
+  }
 
-float USSensorWrapper::scan() {
-  auto reading = us_sensor->distance_centimeters();
-  return reading;
+SensorReading USSensorWrapper::scan() {
+  // To save power (& time) rotating sensor back to other side, we will do a sweep from whichever
+  // side we left off. E.g. L->R and then R->L (see `invert` below).
+  auto readings = std::array<float, num_measurements>{};
+  bool invert = motor->position() > 0;
+  for (unsigned i = 0; i < num_measurements; ++i) {
+    std::this_thread::sleep_for(std::chrono::milliseconds(50));
+    readings.at(invert ? num_measurements - 1 - i : i) = us_sensor->distance_centimeters();
+    std::this_thread::sleep_for(std::chrono::milliseconds(50));
+    if (i + 1 < num_measurements) rotate(invert);
+    std::cout << "Reading done at: " << motor->position() << "\n";
+  }
+  Assert(abs(motor->position()) == abs(m_start_pos), "Motor position after scan invalid.");
+  return readings;
 }
 
-void USSensorWrapper::rotate() {
-  // TODO: Need to figure out logic re how the rotation works.
-  //       To be more efficient start from one end and move to the other every time.
-  //       Need to detect where we left off to go back the next reading. (L->R, then R->L).
-  //       Instead of using the wheel encoder reading twice we could store the value in a short...
-  int constexpr speed = 10;
-  unsigned constexpr mov_dur_ms = 100;
-  motor->set_speed_sp(speed).set_time_sp(mov_dur_ms).run_timed();
+void USSensorWrapper::rotate(bool invert) {
+  // Default is clockwise. Set `invert` to go counter-clockwise.
+  static_assert(m_pos_inc * (num_measurements - 1) == 180, "Sensor inc and num_entries do not match...");
+  motor->set_position_sp(invert ? -m_pos_inc : m_pos_inc);  //  +/- angle change
+  motor->set_speed_sp(invert ? -m_speed : m_speed).run_to_rel_pos();
+  while (motor->state().count("running"))
+    std::this_thread::sleep_for(std::chrono::milliseconds(10));
 }
 
 }  // namespace observation
