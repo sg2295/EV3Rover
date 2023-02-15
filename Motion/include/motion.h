66d9855740902ca4ac55327d6377bf76d74c0be5
Sergios Gavriilidis <sgavriilidis@hotmail.com>
2023-02-15 22:16:49 +0000

    Add asserts for motion

--- Motion/include/motion.h
+++ Motion/include/motion.h
@@ -1,3 +1,6 @@
+#ifndef _MOTION_INCLUDE_MOTION_H_
+#define _MOTION_INCLUDE_MOTION_H_
+
 #include <memory>
 
 namespace ev3dev {
@@ -22,6 +25,11 @@ class MotorsWrapper {
  private:
   std::unique_ptr<ev3dev::large_motor> l_motor;
   std::unique_ptr<ev3dev::large_motor> r_motor;
+  static unsigned constexpr mov_dur_ms = 1000;
+  static unsigned constexpr dflt_speed = 500;
+  static float constexpr turn_s_dec = 0.5;  // % amount by which speed is reduced for a turn...
 };
 
 }  // namespace motion
+
+#endif  // _MOTION_INCLUDE_MOTION_H_
--- Motion/motion.cpp
+++ Motion/motion.cpp
@@ -6,18 +6,22 @@
 #include "motion.h"
 #include "ev3dev.h"
 
+#include "report.h"
+
 namespace motion {
 
 using LM = ev3dev::large_motor;
 
 MotorsWrapper::MotorsWrapper(ev3dev::address_type l_add, ev3dev::address_type r_add) :
-  l_motor{std::make_unique<LM>(l_add)}, r_motor{std::make_unique<LM>(r_add)} {}
+  l_motor{std::make_unique<LM>(l_add)}, r_motor{std::make_unique<LM>(r_add)} {
+    Assert(r_motor->connected(), "Right motor not connected");
+    Assert(l_motor->connected(), "Left motor not connected");
+  }
 
 void MotorsWrapper::drive(Direction dir) {
-  unsigned constexpr mov_dur_ms = 1000;
-  int const speed = (dir != Direction::S) ? 500 : -500;
-  l_motor->set_speed_sp((dir != Direction::NW) ? speed : (speed * 0.50)).set_time_sp(mov_dur_ms).run_timed();
-  r_motor->set_speed_sp((dir != Direction::NE) ? speed : (speed * 0.50)).set_time_sp(mov_dur_ms).run_timed();
+  int const speed = (dir != Direction::S) ? dflt_speed : -dflt_speed;
+  l_motor->set_speed_sp((dir != Direction::NW) ? speed : (speed * turn_s_dec)).set_time_sp(mov_dur_ms).run_timed();
+  r_motor->set_speed_sp((dir != Direction::NE) ? speed : (speed * turn_s_dec)).set_time_sp(mov_dur_ms).run_timed();
   while (l_motor->state().count("running") || r_motor->state().count("running"))
     std::this_thread::sleep_for(std::chrono::milliseconds(10));
 }
