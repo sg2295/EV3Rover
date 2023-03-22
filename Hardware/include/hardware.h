b977d3de6efd59f7aedbe119529bc2077df67d73
Sergios Gavriilidis <sgavriilidis@hotmail.com>
2023-03-22 22:07:59 +0000

    Overload op<< for sensing prepare to save to file

--- Hardware/include/hardware.h
+++ Hardware/include/hardware.h
@@ -5,6 +5,7 @@
 #include <array>
 #include <utility>
 #include <deque>
+#include <iostream>
 
 #include "hardware-scope.h"
 
@@ -29,6 +30,18 @@ struct Sensing {
 
   std::array<float, HardwareConstants::NUM_READINGS> observation;
   Pose odometry;
+
+  friend std::ostream& operator<<(std::ostream& os, Sensing const& s) {
+    std::ignore = s;
+    // Format: [pose], [observation] -> x, y, theta s1 s2 [...] s9
+    os << s.odometry.x << ' ' << s.odometry.y << ' ' << s.odometry.theta;
+    for (auto const obs : s.observation)
+      os << ' ' << obs;
+    // TODO: Do we want to keep flushing after each line, or should we flush only in the end?
+    //       (To avoid any overheads...)
+    os << std::endl;
+    return os;
+  }
 };
 
 class EV3 {
@@ -38,12 +51,12 @@ class EV3 {
   ~EV3();
 
   Sensing operator()(Direction d);
-  std::deque<Sensing> const& get_readings() const { return readings; }
+  // std::deque<Sensing> const& get_readings() const { return readings; }
 
  private:
   std::unique_ptr<observation::USSensorWrapper> us_sensor;
   std::unique_ptr<motion::MotorsWrapper> motors;
-  std::deque<Sensing> readings;
+  // std::deque<Sensing> readings;  // We're saving to file instead...
 };
 
 }  // namespace hardware
