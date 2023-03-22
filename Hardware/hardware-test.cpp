d2c2df062a0b23a175b74963d2232f7ab6688d5f
Sergios Gavriilidis <sgavriilidis@hotmail.com>
2023-03-22 23:32:16 +0000

    Save odometry and observations to file

--- Hardware/hardware-test.cpp
+++ Hardware/hardware-test.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 
 #include "hardware.h"
 
@@ -20,18 +21,23 @@ int main() {
   //   motors.drive(static_cast<Direction>(mc));
   // }
   Log("Hardware test...\n");
+  std::ofstream ofile{"outfile.txt"};
   EV3 ev3{ev3dev::OUTPUT_D, ev3dev::OUTPUT_A, ev3dev::OUTPUT_C, ev3dev::INPUT_1};
   unsigned c = 0;
-  while (true) {
+  while (c < 5) {  // Quick hack to get things working
     Log("Motion command: ");
     std::cin >> c;
     Log('\n');
     auto s = ev3(static_cast<Direction>(c));
+    ofile << s;  // Save to file...
     Log("Odometry: x=", s.odometry.x, ", y=", s.odometry.y, ", theta=", s.odometry.theta, '\n');
     Log("Sensing: ");
     for (float f : s.observation)
       Log(f, ' ');
     Log('\n');
   }
+  ofile << std::endl;
+  ofile.close();
+  Log("Successfully closed the outfile\n");
   return 0;
 }
