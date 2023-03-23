3545d23305ffcb10a36ef8a7616e62a120783131
Sergios Gavriilidis <sgavriilidis@hotmail.com>
2023-03-23 13:55:06 +0000

    change input order

--- Hardware/hardware-test.cpp
+++ Hardware/hardware-test.cpp
@@ -25,15 +25,14 @@ int main() {
   EV3 ev3{ev3dev::OUTPUT_D, ev3dev::OUTPUT_A, ev3dev::OUTPUT_C, ev3dev::INPUT_1};
   unsigned c = 0;
   while (c < 5) {  // Quick hack to get things working
-    Log("Motion command: ");
-    std::cin >> c;
-    Log('\n');
     auto s = ev3(static_cast<Direction>(c));
     ofile << s;  // Save to file...
     Log("Odometry: x=", s.odometry.x, ", y=", s.odometry.y, ", theta=", s.odometry.theta, '\n');
     Log("Sensing: ");
     for (float f : s.observation)
       Log(f, ' ');
+    Log("\nMotion command: ");
+    std::cin >> c;
     Log('\n');
   }
   ofile << std::endl;
