0457ae6d3cdf824e64c113b4a644021adaa1b2da
Sergios Gavriilidis <sgavriilidis@hotmail.com>
2023-08-11 15:59:24 +0000

    Add dummy ev3dev bindings for observation tests

--- Tests/ev3dev-dummy/ev3dev.h
+++ Tests/ev3dev-dummy/ev3dev.h
@@ -7,35 +7,58 @@ namespace ev3dev {
 
 using address_type = std::string;
 
-constexpr char OUTPUT_A[] = "ev3-ports::outA";
+constexpr char INPUT_1[]  = "ev3-ports:in1";
+constexpr char INPUT_2[]  = "ev3-ports:in2";
+
+constexpr char OUTPUT_A[] = "ev3-ports:outA";
 constexpr char OUTPUT_B[] = "ev3-ports:outB";
 constexpr char OUTPUT_C[] = "ev3-ports:outC";
+constexpr char OUTPUT_D[] = "ev3-ports:outD";
 
 struct common {
-  common(address_type addr) : addr{addr} {}
-  // We assume output C is not connected
-  // TODO: will need to add INPUT_C too for observation
-  bool connected() const { return addr != OUTPUT_C; }
+  common(address_type addr) {
+    // We assume both input/output C are not connected
+    is_connected = addr != OUTPUT_C || addr != INPUT_2;
+  }
 
-  common& set_speed_sp(int speed) { (void) speed; return *this; }
-  common& set_time_sp(int time) { (void) time; return *this; }
-  common& run_timed() { return *this; }
   common& state() { return *this; }
-  static bool count(std::string str) { (void) str; return false; }
 
-  address_type addr{};
+  bool connected() const { return is_connected; }
+
+  static bool count(std::string str) { (void)str; return false; }
+
+  bool is_connected;
+};
+
+struct motor_common {
+  motor_common& run_timed() { return *this; }
+  motor_common& run_to_abs_pos() { return *this; }
+  motor_common& run_to_rel_pos() { return *this; }
+
+  motor_common& set_position_sp(int pos) { (void)pos; return *this; }
+  motor_common& set_speed_sp(int speed) { (void)speed; return *this; }
+  motor_common& set_time_sp(int time) { (void)time; return *this; }
+
+  static int position() { return pos; }
+
+  static constexpr int pos = -90;
 };
 
 struct ultrasonic_sensor : public common {
   ultrasonic_sensor(address_type address) : common{address} {}
+
+  static float distance_centimeters() { return dist; }
+
+  static constexpr float dist = 23.9;
 };
 
-struct large_motor : public common {
+struct large_motor : public common, motor_common {
   large_motor(address_type address) : common{address} {}
 };
 
-// TODO: Add dummy versions for observation components
-struct medium_motor;
+struct medium_motor : public common, motor_common {
+  medium_motor(address_type address) : common{address} {}
+};
 
 }  // namespace ev3dev
 
